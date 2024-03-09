93dbd1a098ea2a3e469923eea14fc0418a8d9f02
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2024-03-09 19:55:22 +0530

    changes in index.js

--- JobApp/app/index.js
+++ JobApp/app/index.js
@@ -1,9 +1,30 @@
-import {Text,View} from 'react-native';
+import { useState } from 'react';
+import {Text,View,ScrollView,SafeAreaView} from 'react-native';
+import {Stack, useRouter} from 'expo-router';
+
+import {COLORS, images, icons, SIZES} from '../constants';
+import {Nearbyjobs, Popoularjobs,ScreenHeaderBtn, Welcome } from '../components';
 
 const Home=()=>{
-    <View>
-        <Text>Home</Text>
-    </View>
+
+    const router=useRouter();
+
+    return(
+    <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite}}>
+        <Stack.Screen
+        options={{
+            headerStyle:{backgroundColor:COLORS.lightWhite},
+            headerShadowVisible: false,
+            headerLeft:()=>(
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+            ),
+            headerRight:()=>(
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="100%"/>
+            ),
+        }}
+        />
+    </SafeAreaView>
+    )
 }
 
 export default Home;
