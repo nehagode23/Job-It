f36ff47cad44a333da1ab069dc7dd986a42f2485
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2024-03-23 19:49:35 +0530

    jobs pages

--- JobApp/components/home/popular/Popularjobs.jsx
+++ JobApp/components/home/popular/Popularjobs.jsx
@@ -8,12 +8,36 @@ import PopularJobCard from '../../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
   const router=useRouter();
-
+  const isloading=false;
+  const error=false;
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isloading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary}/>
+        ):error?(
+          <Text>Something went wrong</Text>
+        ):(
+          <FlatList
+          data={[1,2,3,4]}
+          renderItem={()=>(
+            <PopularJobCard
+            item={item}
+            />
+          )}
+          keyExtractor={item=>item?.job_id}
+          contentContainerStyle={{columnGap:SIZES.medium}}
+          />
+
+        )}
       </View>
     </View>
   )
